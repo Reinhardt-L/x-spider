cab5f64526510a970b29ec0dfdd83cb06c9d96c1
Reinhardt-L <65754243+Reinhardt-L@users.noreply.github.com>
2024-05-19 10:37:19 +0800

    Update api.ts

--- src/twitter/api.ts
+++ src/twitter/api.ts
@@ -472,18 +472,17 @@ export async function getUserTweets(
   };
 
   const twitterPosts = extractTwitterPosts(pathToInstructions, resp.body);
-
+  const nextCursor = extractNextCursor(pathToInstructions, resp.body);
   if (!twitterPosts || twitterPosts.length === 0) {
     return {
-      cursor: null,
+      cursor: nextCursor,
       twitterPosts: [],
     };
   }
 
   log.info('twitterPosts', twitterPosts);
 
-  const nextCursor = extractNextCursor(pathToInstructions, resp.body);
-
+  
   return {
     twitterPosts,
     cursor: nextCursor,
